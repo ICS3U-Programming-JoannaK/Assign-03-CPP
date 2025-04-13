fa6337f47dc38a5366cc07870d2ad7a4fb95441f
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-04-13 04:43:34 +0000

    fare.cpp

--- fare.cpp
+++ fare.cpp
@@ -16,9 +16,6 @@ int main() {
     const float OC_STUDENT = 4.00;
     const float OC_SENIOR = 3.20;
     const float OC_ADULT = 4.00;
-    const float TTC_STUDENT = 2.40;
-    const float TTC_SENIOR = 2.30;
-    const float TTC_ADULT = 3.35;
 
     // declare the variables
     std::string ageString;
@@ -40,9 +37,10 @@ int main() {
     std::cout << "Enter your age: ";
     std::cin >> ageString;
 
-    // Convert the age string to an integer
+    // CASTs the age string to an integer
     try {
         ageInteger = std::stoi(ageString);
+        // Checks if the user's age is lower than 1
         if (ageInteger < 1) {
             std::cout << "\nPlease enter a valid age" << std::endl;
         }
@@ -50,8 +48,9 @@ int main() {
         std::cout << "Which transit system are you using today: ";
         std::cin >> busSystemString;
         // Ask the user for the day of the week
-        std::cout << "What day is today (eg; Monday, Thursday): ";
+        std::cout << "What day is today: ";
         std::cin >> dayString;
+        // Makes sure the input for the day is correct
         if (dayString != "Monday" &&
             dayString != "Tuesday" &&
             dayString != "Wednesday" &&
@@ -69,13 +68,16 @@ int main() {
                     std::cout << "\nThere is no fare! have a great day!"
                               << std::endl;
                 } else if (ageInteger > 10 && ageInteger < 65) {
+                    // People between 10 and 65 have to pay the fare
                     std::cout << std::fixed << std::setprecision(2);
                     std::cout << "\nYour fare is $ " << OC_ADULT << std::endl;
                 } else if (ageInteger >= 65) {
+                    // If it is wednesday or sunday, seniors dont have to pay
                     if (dayString == "Wednesday" || dayString == "Sunday") {
                         std::cout << "\nThere is no fare, have a great day"
                                   << std::endl;
                     } else {
+                        // If it is any day but wednesday and sunday
                         std::cout << std::fixed << std::setprecision(2);
                         std::cout << "\nThe fare is $ " << OC_SENIOR << std::endl;
                     }
@@ -86,7 +88,7 @@ int main() {
     } catch (std::invalid_argument) {
         std::cout << ageString << " is not a valid input" << std::endl;
     }
-    // This will display after everything
+    // This will display after everything,
     std::cout << "\n";
     std::cout << "Thank you for using my program \n";
     std::cout << "I hope you have a wonderful day !\n";
